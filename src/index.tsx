6333123250e2e8ff491015d4e5a99fe44ee1355d
Svyatoslav <svyatoslavratov@yandex.ru>
2020-02-15 20:50:50 +0300

    + add testing styles for npm

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 
+import "./style.scss";
+
 export const Hello = () => {
   return (
-    <div>Hello, npm!</div>
-  )
-}
+    <div className="test-style">
+      <div>Hello, npm!</div>
+      <div>Block with style</div>
+    </div>
+  );
+};
 
 export default Hello;
