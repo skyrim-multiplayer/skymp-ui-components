4a6c8e008a77301a1e311306f5811e8df3f3e939
Svyatoslav <svyatoslavratov@yandex.ru>
2020-03-30 22:07:46 +0300

    + add tests with snapshoots for eye-icon

--- src/icons/eye/eye-icon.test.tsx
+++ src/icons/eye/eye-icon.test.tsx
@@ -1,10 +1,21 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
+import { mountToJson } from "enzyme-to-json";
 
 import { SkympEyeIcon } from "./index";
 
 describe("SkympEyeIcon", () => {
   it("renders without errors", () => {
     shallow(<SkympEyeIcon />);
+  });
+
+  it("`isOpen` prop is true", () => {
+    const wrapper = mount(<SkympEyeIcon isOpen={true} />);
+    expect(mountToJson(wrapper)).toMatchSnapshot();
+  });
+
+  it("`isOpen` prop is false", () => {
+    const wrapper = mount(<SkympEyeIcon isOpen={false} />);
+    expect(mountToJson(wrapper)).toMatchSnapshot();
   });
 });
