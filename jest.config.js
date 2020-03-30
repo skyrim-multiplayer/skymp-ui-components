8b0aa38941dcf64b4eeb38cefff588b00d3ded2b
Svyatoslav <svyatoslavratov@yandex.ru>
2020-03-30 22:04:51 +0300

    + add enzyme-to-json, also fix env in config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
   moduleNameMapper: {
     "\\.(css|less|sass|scss)$": "<rootDir>/__mocks__/styleMock.ts",
     "\\.(gif|ttf|eot|svg)$": "<rootDir>/__mocks__/fileMock.ts"
