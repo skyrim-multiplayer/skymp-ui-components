25a8881d8e9bd506e9c2c7c96fb5997c8ec32c03
Svyatoslav <svyatoslavratov@yandex.ru>
2020-03-30 22:06:08 +0300

    + add tests with snapshoots for checkbox-icon

--- src/icons/checkbox/checkbox-icon.test.tsx
+++ src/icons/checkbox/checkbox-icon.test.tsx
@@ -1,10 +1,21 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
+import { mountToJson } from "enzyme-to-json";
 
 import { SkympCheckboxIcon } from "./index";
 
 describe("SkympCheckboxIcon", () => {
   it("renders without errors", () => {
     shallow(<SkympCheckboxIcon />);
+  });
+
+  it("`isChecked` prop is true", () => {
+    const wrapper = mount(<SkympCheckboxIcon isChecked={true} />);
+    expect(mountToJson(wrapper)).toMatchSnapshot();
+  });
+
+  it("`isChecked` prop is false", () => {
+    const wrapper = mount(<SkympCheckboxIcon isChecked={false} />);
+    expect(mountToJson(wrapper)).toMatchSnapshot();
   });
 });
