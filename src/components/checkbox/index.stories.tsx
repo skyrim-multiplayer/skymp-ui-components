c616adcb24d2b58995e68966effbed33330e68b8
Svyatoslav <svyatoslavratov@yandex.ru>
2020-04-01 22:06:42 +0300

    + add Checkbox component, also fix CheckboxIcon

---
+++ src/components/checkbox/index.stories.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+import { Checkbox } from "./index";
+
+export default {
+  title: "Components/Checkbox",
+  component: Checkbox,
+  parameteres: {
+    jest: ["checkbox.test.tsx"]
+  }
+};
+
+const Wrapper = ({ children }: { children: React.ReactChild }) => {
+  return (
+    <div
+      style={{
+        padding: "1rem",
+        background:
+          "rgba(var(--skymp-background-color), var(--skymp-background-opacity))"
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export const Default = () => {
+  return (
+    <Wrapper>
+      <Checkbox onChange={e => console.log("onChange: ", e)}>Label</Checkbox>
+    </Wrapper>
+  );
+};
+
+export const Checked = () => {
+  return (
+    <Wrapper>
+      <Checkbox checked>Label</Checkbox>
+    </Wrapper>
+  );
+};
+
+export const DirectionRight = () => {
+  return (
+    <Wrapper>
+      <Checkbox checkmarkPosition="right">Label</Checkbox>
+    </Wrapper>
+  );
+};
+
+export const Disabled = () => {
+  return (
+    <Wrapper>
+      <Checkbox disabled>Label</Checkbox>
+    </Wrapper>
+  )
+}
---
+++ src/components/checkbox/index.tsx
@@ -0,0 +1,60 @@
+import React, { useCallback, useState } from "react";
+import classNames from "classnames";
+
+import { CheckboxIcon } from "../../icons/checkbox";
+
+import "./style.scss";
+
+export interface CheckboxProps extends React.HTMLProps<HTMLInputElement> {
+  checkmarkPosition?: "left" | "right";
+}
+
+export const Checkbox = React.forwardRef(
+  (
+    {
+      checkmarkPosition = "left",
+      className,
+      style,
+      children,
+      onChange,
+      ...props
+    }: CheckboxProps,
+    ref: React.Ref<HTMLInputElement>
+  ) => {
+    const [isChecked, setIsChecked] = useState(props.checked);
+
+    const changeHandler = useCallback(
+      (e: React.ChangeEvent<HTMLInputElement>) => {
+        setIsChecked(e.target.checked);
+        if (onChange) onChange(e);
+      },
+      [onChange]
+    );
+
+    return (
+      <label
+        className={classNames(
+          "skymp-checkbox",
+          className,
+          `skymp-checkbox_${checkmarkPosition}`,
+          { "skymp-checkbox_disabled": props.disabled }
+        )}
+        style={style}
+      >
+        <CheckboxIcon isChecked={isChecked} className="skymp-checkbox__icon" />
+        <input
+          ref={ref}
+          {...props}
+          type="checkbox"
+          onChange={changeHandler}
+          style={{ display: "none" }}
+        />
+        {children && (
+          <span className="skymp-checkbox__children">{children}</span>
+        )}
+      </label>
+    );
+  }
+);
+
+Checkbox.displayName = "SkympCheckbox";
